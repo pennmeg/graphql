1e66ea915643b4400c6ca1f670dcf37e6326db43
Megan Penn <penn.meg@gmail.com>
2021-03-02 19:26:59 -0500

    Seed data

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,54 +1,63 @@
-import { config, createSchema } from '@keystone-next/keystone/schema'
-import { createAuth } from '@keystone-next/auth'
-import { User } from './schemas/User'
-import { Product } from './schemas/Product'
-import { ProductImage } from './schemas/ProductImage'
-import 'dotenv/config'
-import { withItemData, statelessSessions } from '@keystone-next/keystone/session'
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+import 'dotenv/config';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { insertSeedData } from './seed-data';
 
-
-const databaseURL = process.env.DATABASE_URL // TODO: Add fall back
+const databaseURL = process.env.DATABASE_URL; // TODO: Add fall back
 
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 30, // How long is session
   secret: process.env.COOKIE_SECRET,
-}
+};
 
 const { withAuth } = createAuth({
   listKey: 'User',
   identityField: 'email',
   secretField: 'password',
   initFirstItem: {
-    fields: ['name', 'email', 'password']
+    fields: ['name', 'email', 'password'],
     // TODO: Add roles
-  }
-})
+  },
+});
 
-export default withAuth(config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
     },
-  },
-  db: {
-    adapter: 'mongoose', 
-    url: databaseURL,
-    // TODO: add data seeding here
-  },
-  lists: createSchema({
-    // Schema goes in here
-    User,
-    Product,
-    ProductImage,
-  }),
-  ui: {
-    // Show the UI only for ppl who have a session + logged in
-    isAccessAllowed: ({ session }) => {
-      return !!session?.data
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      async onConnect(keystone) {
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
-  },
-  session: withItemData(statelessSessions(sessionConfig), {
-    User: `id`
+    lists: createSchema({
+      // Schema goes in here
+      User,
+      Product,
+      ProductImage,
+    }),
+    ui: {
+      // Show the UI only for ppl who have a session + logged in
+      isAccessAllowed: ({ session }) => {
+        return !!session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: `id`,
+    }),
   })
-}))
+);
